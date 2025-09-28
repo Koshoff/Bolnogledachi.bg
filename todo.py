9d037bf939f75829f0721c6c45301246a0122f1a
Yordan Yordanov (S. T. G.) <y.yordanov.stg@gmail.com>
2025-09-28 19:31:16 +0300

    Update footer.html

--- todo.py
+++ todo.py
@@ -1,5 +1,6 @@
 # TODO: On Production Need To Config -> ALLOWED_HOSTS, STATIC_ROOT, MEDIA_ROOT
 #       contact_success.html & 404.html & comming_soon.html
+#       <li><a href="about_us.html#mission">Мисия и Ценности</a></li> ????
 #       captcha
 #       rate limit
 #       email backend
